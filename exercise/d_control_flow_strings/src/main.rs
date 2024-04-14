f64a951e9d033aaa2edd62d35dbbda7f54c8e6a8
Andrei - Doru Pata <andrei.pata@hudl.com>
2024-04-14 20:24:13 +0200

    Rust: Utimate Rust Crush Course: Exercise b-e

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -14,6 +14,13 @@ fn main() {
 
     // This consumes the `args` vector to iterate through each String
     for arg in args {
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg);
+        }
         // 1a. Your task: handle the command-line arguments!
         //
         // - If arg is "sum", then call the sum() function
@@ -28,6 +35,9 @@ fn main() {
 
 fn sum() {
     let mut sum = 0;
+    for i in 7..=23 {
+        sum += i;
+    }
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
@@ -39,6 +49,12 @@ fn sum() {
 fn double() {
     let mut count = 0;
     let mut x = 1;
+
+    while x <= 500 {
+        x *= 2;
+        count += 1;
+    }
+
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
@@ -53,6 +69,15 @@ fn count(arg: String) {
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
 
+    let mut count = 0;
+    loop {
+        if count == 8 {
+            break;
+        }
+
+        println!("{}", arg);
+        count += 1;
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
