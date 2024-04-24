c9fe8223de89dfdca1857948a21f0003751832d5
Andrei - Doru Pata <andrei.pata@hudl.com>
2024-04-24 06:06:17 +0300

    Rust: Ultimate Rust Crush Course: Invaders: Start and wait for quit event

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,5 +1,21 @@
-use std::error::Error;
+// standard library imports
+use std::{
+    io,
+    error::Error,
+    time::Duration
+};
+
+// crossterm library imports
+use crossterm::{
+    ExecutableCommand,
+    event::{self, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    cursor::{Hide, Show},
+};
+
+// other imports
 use rusty_audio::Audio;
+use crate::event::Event;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -9,10 +25,33 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("pew", "pew.wav");
     audio.add("startup", "startup.wav");
     audio.add("win", "win.wav");
-
     audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
 
     // Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
     Ok(())
 }
